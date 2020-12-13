cdd031c9e133105d50b96621d534b873506f6d8a
alantoledo007 <alantoledo007@gmail.com>
2020-12-13 00:30:58 -0300

    corrección de configuración de tailwind (provocaba error en el build)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  purge: false,
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.js',
+      'layouts/**/*.js',
+      'pages/**/*.js',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+    options: {
+      whitelistPatterns: [/agile*/],
+      whitelistPatternsChildren: [/agile*/]
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
